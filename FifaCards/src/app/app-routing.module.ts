e2983bd3d6852628bbce9227d91c9da8ca3c16c4
Theohrt23 <theo.huret@epitech.eu>
2023-01-08 17:09:39 +0100

    feat(app-routing.module.ts) add route

--- FifaCards/src/app/app-routing.module.ts
+++ FifaCards/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardsInfoComponent } from './cards-info/cards-info.component';
+import { CardsListComponent } from './cards-list/cards-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: CardsListComponent },
+  { path: 'info', component: CardsInfoComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
