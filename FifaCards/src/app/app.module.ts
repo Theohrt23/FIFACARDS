9e4eff2ae24a0ab7946028f102a7eac3bfbe3dff
Theohrt23 <theo.huret@epitech.eu>
2023-01-08 17:01:23 +0100

    feat(index.html/app-component.html) create html page

--- FifaCards/src/app/app.module.ts
+++ FifaCards/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsListComponent } from './cards-list/cards-list.component';
+import { CardsInfoComponent } from './cards-info/cards-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsListComponent,
+    CardsInfoComponent
   ],
   imports: [
     BrowserModule,
---
+++ FifaCards/src/app/cards-info/cards-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-info',
+  templateUrl: './cards-info.component.html',
+  styleUrls: ['./cards-info.component.css']
+})
+export class CardsInfoComponent {
+
+}
